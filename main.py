1b3c850adfbd78bcaf78726bb1ba5440ee556556
Levente Ibranyi <ibranyilevente@gmail.com>
2021-06-09 22:50:37 +0200

    NameCheck method add

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ def DataIn(point, name): #point and name input
     line = str(point) + " - " + name
     return line
 
+def NameCheck(name):
+    if name == "":
+        print("Your name is required")
+        return [False]
+    if len(name) > 10:
+        print("Your name cannot be longer than 10 characters")
+        return [False]
+    return [True, "Ok"]
+
+
 playersList = []
 
 def main():
@@ -22,6 +32,9 @@ def main():
                 playersList.append(playerObj)
 
     name = input("Add meg a nevedet: ")
+    while(not NameCheck(name)[0]):
+        name = input("Add meg újra a nevedet: ")
+
     point = int(input("Add meg a pontodat: "))
     f = open("score.txt", "a", encoding="UTF-8")
     f.write(DataIn(point, name) + "\n")
